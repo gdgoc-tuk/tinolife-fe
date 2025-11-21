3b6a1a9bf5d3a383ea625fe3ef942e9e8c8919bd
gs0428 <lider28@naver.com>
2025-11-21 19:26:34 +0900

    rename: 폴더 이름 수정

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 
-import GetStartedSection from "./components/get-started-section";
+import GetStartedSection from "./_components/get-started-section";
 
 const logo = "/static/logo.png";
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col justify-between pb-10">
+    <main className="flex flex-col justify-between pb-4">
       <section className="mx-auto flex flex-1 flex-col items-center justify-center space-y-6">
         <Image src={logo} alt="logo" width={133} height={139} />
         <div className="space-y-2 text-center">
